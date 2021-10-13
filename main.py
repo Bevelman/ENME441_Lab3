f3983e62e8fdb86aeae78a822dbda7bdb288781d
Bevelman <91498634+Bevelman@users.noreply.github.com>
2021-10-13 16:50:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+#   To check address: sudo i2cdetect -y 1
+
+import smbus
+import time
+
+class PCF8591:
+
+  def __init__(self,address):
+    self.bus = smbus.SMBus(1)
+    self.address = address
+
+  def read(self,chn): #channel
+      try:
+          self.bus.write_byte(self.address, 0x40 | chn)  # 01000000
+          self.bus.read_byte(self.address) # dummy read to start conversion
+      except Exception as e:
+          print ("Address: %s \n%s" % (self.address,e))
+      return self.bus.read_byte(self.address)
+
+  def write(self,val):
+      try:
+          self.bus.write_byte_data(self.address, 0x40, int(val))
+      except Exception as e:
+          print ("Error: Device address: 0x%2X \n%s" % (self.address,e))
+
+class Joystick:
+  def __init__(self,address):
+    self.PCF8591 = PCF8591(address)
+  def getX(self,xchn):
+    return self.read(self,xchn)
+  def getY(self,ychn):
+    return self.read(self,ychn)
+
+theJoystick=Joystick(0x48)
+while True:
+  print(theJoystick.getX(0x0) +",\t" + theJoystick.getY(0x1))
+  time.sleep(0.1)
