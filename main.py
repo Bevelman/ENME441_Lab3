e5c5f604450b69f1ec90fcb674280310518c85d8
Bevelman <91498634+Bevelman@users.noreply.github.com>
2021-10-13 19:01:54 +0000

    ethety

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class Joystick:
     self.xchn = xchn
     self.ychn = ychn
   def getX(self,xchn):
-    return self.PCF8591.read(self,xchn)
+    return self.read(self,xchn)
   def getY(self,ychn):
     return self.PCF8591.read(self,ychn)
 
